700a93001cbeb5d6b9e269d82ef2d8f9d1266ed2
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-11-10 19:21:56 +0600

    article added

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import Card from './Service/Card';
+import video from './../Resources/Large-Img/20220807_195817_1.mp4'
 
 const Home = () => {
 
@@ -61,6 +62,26 @@ const Home = () => {
 
             <button className='m-auto my-10 text-red-500 w-full'><Link to='/service'>See all</Link></button>
 
+
+            {/* other detail parts */}
+            <article className='grid grid-cols-2 gap-3 w-10/12 m-auto'>
+                <div>
+                    <h1 className='text-lg font-bold'>About me!</h1>
+                    <p>I am a  professional athlete and i have experience of more than 10 years in this field. During this period I learned many new things, experienced many kinds of positivity as well as negativity. I have experience of rock climbing, adventure racing, swimming, climbing, and so on. I have many national as well as international achievements. I personally enjoy this field and encourage others to explore this field. </p>
+                </div>
+                <div>
+                    <img src="https://i.ibb.co/TmtGwMc/cover.jpg" alt="" srcset="" />
+                </div>
+            </article>
+            <article className='grid grid-cols-2 gap-3 w-10/12 m-auto'>
+                <div>
+                    <h1 className='text-lg font-bold'>About me!</h1>
+                    <p> </p>
+                </div>
+                <div>
+                    <video src={video}></video>
+                </div>
+            </article>
         </>
     );
 
