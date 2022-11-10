beea1e4f88c28875b0a5318bdad75bc316ea62e4
Kawsar Ahmed <kawsarmiahstdnt@gmail.com>
2022-11-10 15:57:39 +0600

    service added

--- src/Pages/Service/Service.js
+++ src/Pages/Service/Service.js
@@ -8,9 +8,12 @@ const Service = () => {
             .then(data => setData(data))
     }, [])
     return (
-        <div>
+        <div className='mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             {
-                data.map(details => <Card key={details._id} detail={details}></Card>)
+                data.map(details => <div className='m-auto'>
+                    <Card key={details._id} detail={details}></Card>
+                </div>
+                )
             }
         </div>
     );
@@ -19,22 +22,29 @@ const Service = () => {
 const Card = (details) => {
     const { description, duration, img, isBook, price, ratings, title } = details.detail
     console.log(description, duration, img, isBook, price, ratings, title)
+    const shortDes = description.split(" ")
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src={img} alt="Tammat's service" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">
-                    {title}
-                    <div className="badge badge-secondary">{ratings}</div>
-                </h2>
-                <p>{description}</p>
-                <div>
-                    <p>Price: <span>{price}</span></p>
-                    <p>Duration: <span>{duration}</span></p>
-                </div>
-                <div className="card-actions justify-end">
-                    <div className="badge badge-outline">Details</div>
-                    <div className="badge badge-outline">{isBook}</div>
+        <div>
+            <div className="card w-10/12 h-full bg-base-100 shadow-xl">
+                <figure><img className='mt-0 h-72 w-full' src={img} alt="Tammat's service" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">
+                        {title}
+                        <div className="badge badge-secondary">{ratings}</div>
+                    </h2>
+                    <p className='h-12 text-ellipsis overflow-hidden ...'>{description}</p>
+                    <div className='text-slate-500'>
+                        <p className='font-bold'>Price: <span>{price}</span></p>
+                        <p className='font-bold'>Duration: <span>{duration}</span></p>
+                    </div>
+                    <div className="card-actions justify-end">
+                        <div className="badge badge-outline cursor-pointer px-5 py-3 font-bold">Details</div>
+                        {
+                            isBook ? <div className="badge badge-outline cursor-pointer px-5 py-3 font-bold">Booked</div>
+                                : <div className="badge badge-outline cursor-pointer px-5 py-3 font-bold bg-lime-800 text-white">Book</div>
+                        }
+
+                    </div>
                 </div>
             </div>
         </div>
